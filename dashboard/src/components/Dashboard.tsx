6b6363e8ac1e6bd0c192859e7e748170551d5657
FyZyX <lucasmlofaro@gmail.com>
2024-05-12 02:29:22 -0700

    refactor into data service

--- dashboard/src/components/Dashboard.tsx
+++ dashboard/src/components/Dashboard.tsx
@@ -3,62 +3,80 @@ import MapVisualizer from "./MapVisualizer";
 import LiveMapVisualizer from "./LiveMapVisualizer";
 import StatsTable from "./StatsTable";
 import EntityGrid from "./EntityGrid";
-import {LiveMetrics, Metric, SummaryStats} from "../model";
+import {LiveMetric, LiveMetrics, Metric, SummaryStats} from "../model";
 
 import styles from './Dashboard.module.css';
 
-const apiHost = import.meta.env.VITE_API_HOST || 'localhost';
-const apiPort = import.meta.env.VITE_API_PORT || 8080;
 
 const [liveMetrics, setLiveMetrics] = createSignal<LiveMetrics>({});
 
-function websocketURL(): string {
-    return `ws://${apiHost}:${apiPort}/ws`;
-}
 
-function endpointURL(path: string): string {
-    return `http://${apiHost}:${apiPort}/${path}`;
-}
+class DataService {
+    apiHost = import.meta.env.VITE_API_HOST || 'localhost';
+    apiPort = import.meta.env.VITE_API_PORT || 8080;
 
-function setupWebSocket() {
-    const ws = new WebSocket(websocketURL());
+    top10EndpointURL = this.endpointURL('metrics/top10')
+    summaryStatsEndpointURL = this.endpointURL('summary-stats')
 
-    ws.onopen = () => console.log('WebSocket connected');
-    ws.onclose = () => console.log('WebSocket disconnected');
-    ws.onerror = error => console.error('WebSocket error:', error);
-    ws.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        setLiveMetrics(prev => ({...prev, [data.name]: data}));
+    websocketURL(): string {
+        return `ws://${this.apiHost}:${this.apiPort}/ws`;
+    }
+
+    endpointURL(path: string): string {
+        return `http://${this.apiHost}:${this.apiPort}/${path}`;
+    }
+
+    newSocket(handleMessage: (liveMetric: LiveMetric) => void) {
+        const ws = new WebSocket(this.websocketURL());
+
+        ws.onopen = () => console.log('WebSocket connected');
+        ws.onclose = () => console.log('WebSocket disconnected');
+        ws.onerror = error => console.error('WebSocket error:', error);
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            handleMessage(data);
+        };
+
+        onCleanup(() => {
+            ws.close();
+        });
+
+        return ws;
+    }
+
+    sendSubscription(ws: WebSocket, names: string[]) {
+        ws.send(JSON.stringify({
+            action: 'subscribe',
+            names: names
+        }));
+    }
+
+    sendUnsubscription(ws: WebSocket, names: string[]) {
+        ws.send(JSON.stringify({
+            action: 'unsubscribe',
+            names: names
+        }));
+
+        setLiveMetrics(prevMetrics => {
+            const newMetrics = {...prevMetrics};
+            names.forEach(name => {
+                delete newMetrics[name];
+            });
+            return newMetrics;
+        });
+    }
+
+    fetchSummaryStats = async (): Promise<SummaryStats[]> => {
+        const response = await fetch(this.summaryStatsEndpointURL);
+        return await response.json();
     };
 
-    onCleanup(() => {
-        ws.close();
-    });
-
-    return ws;
+    fetchMetrics = async (): Promise<Metric[]> => {
+        const response = await fetch(this.top10EndpointURL);
+        return await response.json();
+    };
 }
 
-function sendSubscription(ws: WebSocket, names: string[]) {
-    ws.send(JSON.stringify({
-        action: 'subscribe',
-        names: names
-    }));
-}
-
-function sendUnsubscription(ws: WebSocket, names: string[]) {
-    ws.send(JSON.stringify({
-        action: 'unsubscribe',
-        names: names
-    }));
-
-    setLiveMetrics(prevMetrics => {
-        const newMetrics = {...prevMetrics};
-        names.forEach(name => {
-            delete newMetrics[name];
-        });
-        return newMetrics;
-    });
-}
 
 const Dashboard: Component = () => {
     const [metrics, setMetrics] = createSignal<Metric[]>([]);
@@ -66,11 +84,14 @@ const Dashboard: Component = () => {
     const [liveData, setLiveData] = createSignal(false);
     const [ws, setWs] = createSignal<WebSocket | null>(null);
 
-    const top10EndpointURL = endpointURL('metrics/top10')
-    const summaryStatsEndpointURL = endpointURL('summary-stats')
+    const dataService = new DataService();
 
     onMount(() => {
-        setWs(setupWebSocket());
+        const handleMessage = (liveMetric: LiveMetric) => {
+            setLiveMetrics(prev => ({...prev, [liveMetric.name]: liveMetric}));
+        };
+        const socket = dataService.newSocket(handleMessage);
+        setWs(socket);
     });
 
     onCleanup(() => {
@@ -87,23 +108,13 @@ const Dashboard: Component = () => {
         const subscribes = newNames.filter(name => !currentNames.includes(name));
         const unsubscribes = currentNames.filter(name => !newNames.includes(name));
 
-        sendSubscription(socket, subscribes);
-        sendUnsubscription(socket, unsubscribes);
-    };
-
-    const fetchSummaryStats = async (): Promise<SummaryStats[]> => {
-        const response = await fetch(summaryStatsEndpointURL);
-        return await response.json();
-    };
-
-    const fetchMetrics = async (): Promise<Metric[]> => {
-        const response = await fetch(top10EndpointURL);
-        return await response.json();
+        dataService.sendSubscription(socket, subscribes);
+        dataService.sendUnsubscription(socket, unsubscribes);
     };
 
     const updateKeyMetrics = async () => {
-        const newSummaryStats = await fetchSummaryStats();
-        const newMetrics = await fetchMetrics();
+        const newSummaryStats = await dataService.fetchSummaryStats();
+        const newMetrics = await dataService.fetchMetrics();
 
         const currentNames = metrics().map(metric => metric.name);
         const newNames = newMetrics.map(metric => metric.name);
@@ -127,8 +138,10 @@ const Dashboard: Component = () => {
 
     return (
         <main class={styles.dashbaord}>
-            <StatsTable metrics={metrics()}/>
-            <EntityGrid summaryStats={summaryStats()}/>
+            {liveData()
+                ? <EntityGrid summaryStats={summaryStats()}/>
+                : <StatsTable metrics={metrics()}/>
+            }
             <button onClick={() => setLiveData(!liveData())}>Toggle Live Data</button>
             {liveData()
                 ? <LiveMapVisualizer liveMetrics={liveMetrics()}/>
