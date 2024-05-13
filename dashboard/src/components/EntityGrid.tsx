0484d0f02ba0f40ac0fe9a80d2d51083a3edc416
FyZyX <lucasmlofaro@gmail.com>
2024-05-12 20:49:42 -0700

    improve entity grid styling

--- dashboard/src/components/EntityGrid.tsx
+++ dashboard/src/components/EntityGrid.tsx
@@ -3,6 +3,24 @@ import {SummaryStats} from "../model";
 
 import styles from "./EntityGrid.module.css";
 
+interface EntityCardProps {
+    stats: SummaryStats;
+}
+
+const EntityCard: Component<EntityCardProps> = (props) => {
+    return (
+        <div class={styles.summaryStats}>
+            <h3>{props.stats.name}</h3>
+            <div>
+                <p>Mean: {props.stats.mean_measurement.toFixed(1)}</p>
+                <p>Max: {props.stats.max_measurement.toFixed(1)}</p>
+                <p>Min: {props.stats.min_measurement.toFixed(1)}</p>
+                <p>Count: {props.stats.count}</p>
+            </div>
+        </div>
+    );
+};
+
 interface EntityGridProps {
     summaryStats: SummaryStats[];
 }
@@ -11,15 +29,7 @@ const EntityGrid: Component<EntityGridProps> = (props) => {
     return (
         <div class={styles.entityGrid}>
             <For each={props.summaryStats}>
-                {(stats) => (
-                    <div class={styles.summaryStats}>
-                        <h3>{stats.name}</h3>
-                        <p>Mean: {stats.mean_measurement.toFixed(1)}</p>
-                        <p>Max: {stats.max_measurement.toFixed(1)}</p>
-                        <p>Min: {stats.min_measurement.toFixed(1)}</p>
-                        <p>Count: {stats.count}</p>
-                    </div>
-                )}
+                {(stats) => <EntityCard stats={stats}/>}
             </For>
         </div>
     );
